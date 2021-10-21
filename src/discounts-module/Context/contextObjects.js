995eb3847767f8311d919cc242059af0148024d0
gratitudeandfocus <gratitudeandfocus@fastmail.com>
2021-10-21 05:37:43 -0400

    Created Context Object for Headers and Prices

---
+++ src/discounts-module/Context/contextObjects.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+// INITIALIZATION OF CONTEXT OBJECTS - FOR HEADERS
+export const StoreNumberContext = React.createContext('');
+export const StoreAddressContext = React.createContext('');
+export const StorePhoneContext = React.createContext('');
+export const StoreHoursContext = React.createContext('');
+export const StoreExchangePolicyContext = React.createContext('');
+
+// INITIALIZATION OF CONTEXT OBJECTS - FOR PRICES
+export const ChildrensPriceContext = React.createContext('');
+export const HardcoverPriceContext = React.createContext('');
+export const SoftcoverPriceContext = React.createContext('');
+export const AlbumsPriceContext = React.createContext('');
+export const CdsPriceContext = React.createContext('');
+export const DvdsPriceContext = React.createContext('');
+export const SetsPriceContext = React.createContext('');
+export const VHSPriceContext = React.createContext('');
