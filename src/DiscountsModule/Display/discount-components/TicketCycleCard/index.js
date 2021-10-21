da125af0c0187fb96fbcb62591d79f41f69c9c6d
gratitudeandfocus <gratitudeandfocus@fastmail.com>
2021-10-21 15:46:10 -0400

    TicketCycleCard renders color components successfully

--- src/DiscountsModule/Display/discount-components/TicketCycleCard/index.js
+++ src/DiscountsModule/Display/discount-components/TicketCycleCard/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import {Card} from 'reactstrap';
 import {useDaysPassed} from '../../../Hooks/useDaysPassed'
+import * as Ticket from './ColorComponents'
 
 export const TicketCycleCard = (props) => {
     const {color} = useDaysPassed();
     const setColorComponent = () => {
         switch(color){
-            case 'Yellow': return "YELLOW"
-            case 'Green': return "GREEN"
-            case 'Blue': return "BLUE"
-            case 'White': return "WHITE"
-            case 'Pink': return "PINK"
+            case 'Yellow': return <Ticket.Yellow />
+            case 'Green': return <Ticket.Green />
+            case 'Blue': return <Ticket.Blue />
+            case 'White': return <Ticket.White />
+            case 'Pink': return <Ticket.Pink />
             
             default: return "Loading..."
         }
