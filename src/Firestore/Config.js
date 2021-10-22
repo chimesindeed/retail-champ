5c58973bf5d5170ab4edd684f1031353d0795de4
gratitudeandfocus <gratitudeandfocus@fastmail.com>
2021-10-22 02:40:56 -0400

    Added Firestore config

---
+++ src/Firestore/Config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app"
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAYqZx2SaYAQFgfuqsGeOoyX3TM4CvSPjs",
+  authDomain: "retail-companion-e4310.firebaseapp.com",
+  projectId: "retail-companion-e4310",
+  storageBucket: "retail-companion-e4310.appspot.com",
+  messagingSenderId: "783423928675",
+  appId: "1:783423928675:web:ec558cf6cdfbb356efbb56"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
