2acca3b0c472d77f78d2584ef38e41864dc6a09b
gratitudeandfocus <gratitudeandfocus@fastmail.com>
2021-10-23 02:51:59 -0400

    FetchPrices, retrieves, subscribes to and loads into context - item prices from Firestore backend

--- src/index.js
+++ src/index.js
@@ -5,13 +5,14 @@ import './Styles/Styles.css';
 import {App} from './app-entry/App';
 import {ProvideContextForHeaders} from './DiscountsModule/Context/ProvideContextForHeaders'
 import {ProvideContextForPrices} from './DiscountsModule/Context/ProvideContextForPrices'
+import {FetchPrices} from './DiscountsModule/FetchComponents/FetchPrices'
 
 ReactDOM.render(
     <ProvideContextForHeaders>
     <ProvideContextForPrices>
-
+    <FetchPrices>
         <App />
-
+    </FetchPrices>
     </ProvideContextForPrices>
     </ProvideContextForHeaders>,
   document.getElementById('root')
