762c4a1bb1c22cc950952d10bb5034789a68aaca
gratitudeandfocus <gratitudeandfocus@fastmail.com>
2021-10-27 17:36:13 -0400

    Integrated Firebase auth w. email and password

--- src/app-entry/route-components/LoginModuleRoute/index.js
+++ src/app-entry/route-components/LoginModuleRoute/index.js
@@ -1,20 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Button, Form, FormGroup, Label, Input} from 'reactstrap';
+import {auth} from '../../../Firestore/Config'
+import {signInWithEmailAndPassword, onAuthStateChanged, signOut} from 'firebase/auth'
 
 export const LoginModuleRoute = (props) => {
-  return (
-    <Form>
-      <FormGroup>
-        <Label for="email">Email</Label>
-        <Input type="email" name="email" id="email" placeholder="email" />
-      </FormGroup>
-      <FormGroup>
-        <Label for="password">Password</Label>
-        <Input type="password" name="password" id="password" placeholder="password" />
-      </FormGroup>
-      
-      <Button>Submit</Button>
-    </Form>
-  );
+    
+    const [user, setUser] = useState({});
+    const [loginEmail, setLoginEmail] = useState('');
+    const [loginPassword, setLoginPassword] = useState('');
+    
+    onAuthStateChanged(auth, (currentUser)=>{setUser(currentUser)})
+    
+    const login = async () => {
+        try {const user = await signInWithEmailAndPassword(auth, loginEmail, loginPassword);}
+        catch (error){console.log(error)}
+    }
+    
+    const logout = async () => {await signOut(auth);}
+  
+    return (
+        <Form>
+            <FormGroup>
+                <Label for="email">Email</Label>
+                <Input 
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder="email"
+                    onChange={(e)=>{setLoginEmail(e.target.value)}}
+                />
+            </FormGroup>
+    
+            <FormGroup>
+                <Label for="password">Password</Label>
+                <Input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="password"
+                    onChange={(e)=>{setLoginPassword(e.target.value)}}
+                />
+            </FormGroup>
+            <h4>Current User: {user?.email}</h4>
+            <Button onClick={login}>Submit</Button>
+            <Button onClick={logout}>Logout</Button>
+        </Form>
+    );
 }
-
