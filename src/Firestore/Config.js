a1e245a132770e3a0bcb4ad98319d1923efb3188
gratitudeandfocus <gratitudeandfocus@fastmail.com>
2021-10-27 15:58:58 -0400

    Added auth object to Firestore/Config

--- src/Firestore/Config.js
+++ src/Firestore/Config.js
@@ -1,15 +1,18 @@
 import { initializeApp } from "firebase/app"
+import {getAuth} from 'firebase/auth'
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAYqZx2SaYAQFgfuqsGeOoyX3TM4CvSPjs",
-  authDomain: "retail-companion-e4310.firebaseapp.com",
-  projectId: "retail-companion-e4310",
-  storageBucket: "retail-companion-e4310.appspot.com",
-  messagingSenderId: "783423928675",
-  appId: "1:783423928675:web:ec558cf6cdfbb356efbb56"
+    apiKey: "AIzaSyAYqZx2SaYAQFgfuqsGeOoyX3TM4CvSPjs",
+    authDomain: "retail-companion-e4310.firebaseapp.com",
+    projectId: "retail-companion-e4310",
+    storageBucket: "retail-companion-e4310.appspot.com",
+    messagingSenderId: "783423928675",
+    appId: "1:783423928675:web:ec558cf6cdfbb356efbb56"
 };
 
 const app = initializeApp(firebaseConfig);
 
 export default app;
+
+const auth = getAuth(app);
