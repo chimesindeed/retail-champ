f39fdd46e5192a761a2a4588891399b81fff5e8c
gratitudeandfocus <gratitudeandfocus@fastmail.com>
2021-11-01 16:05:08 -0400

    Button toggles display of LoginModule

--- src/App/index.js
+++ src/App/index.js
@@ -1,17 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import {DiscountsModule} from '../Components/DiscountsModule'
 import {LoginModule} from '../Components/LoginModule'
 
 export const App = () => {
+    const handleClick = () => {
+        setLoginNavToggle((prevState)=>!prevState)
+    }
+    const [loginNavToggle, setLoginNavToggle] = useState(false)
     return (
-      <Router>
         <div>
-          <Switch>
-            <Route path='/discounts' exact component={DiscountsModule}/>
-            <Route path='/login' exact component = {LoginModule}/>
-          </Switch>
+            <button className="hamburger-button" onClick={handleClick}>-</button>
+            {
+                loginNavToggle
+                ? <LoginModule />
+                : null
+            }
+            <Router>
+                <Switch>
+                    <Route path='/discounts' exact component={DiscountsModule}/>
+                    <Route path='/login' exact component = {LoginModule}/>
+                </Switch>
+            </Router>
         </div>
-      </Router>
     )
 }
