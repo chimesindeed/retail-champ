7bc4e3607e47a2450d88b146247a1d607af1ce89
gratitudeandfocus <gratitudeandfocus@fastmail.com>
2021-11-01 16:02:00 -0400

    LoginModule working

--- src/Components/LoginModule/index.js
+++ src/Components/LoginModule/index.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {Button, Form, Label, Input} from 'reactstrap';
+import {Button, Form, Input} from 'reactstrap';
 import {signInWithEmailAndPassword, onAuthStateChanged, signOut} from 'firebase/auth'
 import {auth} from '../../Firebase/firebaseConfig'
 
@@ -22,27 +22,29 @@ export const LoginModule = (props) => {
   
     return (
         <Form className="login-form">
-            <Label for="email">Email</Label>
+            <label className="email-label" for="email">email:</label>
             <Input 
-                className="login-input"
+                className="email-input"
                 type="email"
                 id="email"
-                placeholder="email"
                 onChange={(e)=>{setLoginEmail(e.target.value)}}
             />
             
-            <Label for="password">Password</Label>
+            <label className="password-label"for="password">password:</label>
             <Input
-                className="login-input"
+                className="password-input"
                 type="password"
                 id="password"
-                placeholder="password"
                 onChange={(e)=>{setLoginPassword(e.target.value)}}
             />
 
-            <h4>Current User: {user?.email}</h4>
-            <Button onClick={login}>Submit</Button>
-            <Button onClick={logout}>Logout</Button>
+            <Button className="login-button" onClick={login}>login</Button>
+            <Button className="logout-button" onClick={logout}>logout</Button>
+            {
+                user?.email
+                    ? <div className="current-user">logged in: {user?.email}</div>
+                    : null
+            }
         </Form>
     );
 }
