3434f1ff367176fad7d85c314961f6c754a688fa
gratitudeandfocus <gratitudeandfocus@fastmail.com>
2021-11-02 00:21:38 -0400

    Books & Media Components only flip to input field if user logged in

--- src/Components/DiscountsModule/Display/discount-components/ItemizedDiscountsCard/Books/Childrens/index.js
+++ src/Components/DiscountsModule/Display/discount-components/ItemizedDiscountsCard/Books/Childrens/index.js
@@ -1,25 +1,32 @@
- import React, {useState, useContext} from 'react'
- import ReactCardFlip from 'react-card-flip'
- import {Price} from './Price'
- import {Input} from './Input'
- import {CurrentUser} from '../../../../../Context/contextObjects'
- 
- export const Childrens = () => {
-    const [isFlipped, setIsFlipped] = useState(false)
+import React, {useState, useContext} from 'react'
+import ReactCardFlip from 'react-card-flip'
+import {Price} from './Price'
+import {Input} from './Input'
+import {CurrentUser} from '../../../../../Context/contextObjects'
 
-    const handleClick = (e) => {
+export const Childrens = () => {
+    const [isFlipped, setIsFlipped] = useState(false)
+    const [currentUser] = useContext(CurrentUser)
+    
+    const handleClickFromFront = (e) => {
+        if (currentUser?.email){
+            setIsFlipped(prevState => (!prevState));
+        }
+    }
+    
+    const handleClickFromBack = (e) => {
         setIsFlipped(prevState => (!prevState));
     }
     
     return (
             <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical">
-                <div key = 'front' onDoubleClick={handleClick}>
+                <div key = 'front' onDoubleClick={handleClickFromFront}>
                     <Price/>
                 </div>
 
-                <div key="back" onDoubleClick={handleClick}>
+                <div key="back" onDoubleClick={handleClickFromBack}>
                     <Input/>
                 </div>
             </ReactCardFlip>
-        )
-    }
+    )
+}
