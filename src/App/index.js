c69f8a76622274d83df412f167edcb2f464746b9
gratitudeandfocus <gratitudeandfocus@fastmail.com>
2021-11-01 22:36:58 -0400

    minor changes

--- src/App/index.js
+++ src/App/index.js
@@ -1,23 +1,9 @@
-<<<<<<< HEAD
 import React from 'react'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import {DiscountsModule} from '../Components/DiscountsModule'
 import {LoginModule} from '../Components/LoginModule'
 export const App = () => {
        
-=======
-import React, {useState, useContext} from 'react'
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-import {DiscountsModule} from '../Components/DiscountsModule'
-import {LoginModule} from '../Components/LoginModule'
-import {LoginBarToggle} from '../Components/DiscountsModule/Context/contextObjects'
-
-export const App = () => {
-    const handleClick = () => {
-        setLoginNavToggle((prevState)=>!prevState)
-    }
-    const [loginNavToggle, setLoginNavToggle] = useState(LoginBarToggle)
->>>>>>> 2e6505ed607aa1e5b5fbbd181598665218cbd75f
     return (
         <Router>
             <Switch>
