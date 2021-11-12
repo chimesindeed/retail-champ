bc068695ae5886f9afccd6152a3c896ab7fb3af0
gratitudeandfocus <gratitudeandfocus@fastmail.com>
2021-11-12 10:55:07 -0500

    Root route redirects to '/discounts'

--- src/App/index.js
+++ src/App/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom'
 import {DiscountsModule} from '../Components/DiscountsModule'
 import {LoginModule} from '../Components/LoginModule'
 export const App = () => {
@@ -7,6 +7,9 @@ export const App = () => {
     return (
         <Router>
             <Switch>
+                <Route exact path='/'>
+                    <Redirect to='/discounts'/>
+                </Route>
                 <Route path='/discounts' exact component={DiscountsModule}/>
                 <Route path='/login' exact component = {LoginModule}/>
             </Switch>
